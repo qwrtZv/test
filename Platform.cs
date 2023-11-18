d39ef995dc57d9a42ff458cae628c9ad04f489ee
qwrtZv <149222432+qwrtZv@users.noreply.github.com>
2023-11-18 11:31:14 +0600

    Update Platform.cs

--- Platform.cs
+++ Platform.cs
@@ -1,37 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+ 
 public class Platform : MonoBehaviour
 {
+    //Скорость движения платформы
     public float speed;
+    //Направление движения платформы
     public Vector3 direction;
+    //Состояние платформы: активна / не активна
     public bool isActive;
-
+ 
+ 
+    //Обновление движения платформы каждый кадр
     void Update()
     {
+ 
         if (isActive)
         {
+ 
             transform.position += direction * speed * Time.deltaTime;
         }
     }
-    void OnTriggerEnter(Collider other)
-    {
+ 
+ 
+    //Столкновение платформы с двумя типами объектов
+    void OnTriggerEnter(Collider other) {
+        /Если платформа достигает точки остановки, то она меняет направление
+        своего движения/
         if (other.tag == "PlatformStop")
         {
             direction *= -1;
         }
-        if (other.tag == "Player")
+        //Если платформы коснулся игрок, то она активируется
+        if(other.tag == "Player")
         {
             isActive = true;
         }
     }
-    private void OnTriggerExit(Collider other)
-    {
+ 
+    void OnTriggerExit(Collider other) {
+       //Если с платформы ушёл игрок, то она дезактивируется
         if (other.tag == "Player")
         {
             isActive = false;
         }
-    }
+    } 
+ 
 }
